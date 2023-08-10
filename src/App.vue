cf48e62924aa2ce3f0300ee97036a20f77956159
robicse11127 <robizmailbox@gmail.com>
2023-08-10 19:40:45 +0600

    Completed toolbar design

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Toolbar from '@/components/common/Toolbar.vue';
+</script>
+
+<template>
+	<div class="rs__notes-wrapper">
+		<Toolbar />
+	</div>
+</template>
---
+++ src/components/common/Toolbar.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+</script>
+
+<template>
+	<div class="rs__top-toolbar">
+		<div class="rs__toolbar-title">
+			RS Notes
+			<span class="rs__action-icon material-symbols-sharp">edit_note</span>
+		</div>
+		<div class="rs__action-icons">
+			<span class="material-symbols-sharp">delete</span>
+		</div>
+		<div class="rs__action-icons rs__justify-end">
+			<span class="material-symbols-sharp">search</span>
+			<span class="material-symbols-sharp">view_list</span>
+			<span class="material-symbols-sharp">grid_view</span>
+		</div>
+	</div>
+</template>
+
+
