dc390e25cda69f031f8e300b330611e989227996
robicse11127 <robizmailbox@gmail.com>
2023-08-15 08:12:10 +0600

    Ep-4 completed

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup>
 import Toolbar from '@/components/common/Toolbar.vue';
+import ListView from '@/components/list/ListView.vue';
 </script>
 
 <template>
 	<div class="rs__notes-wrapper">
 		<Toolbar />
+		<div class="rs__content-wrapper">
+			<ListView />
+		</div>
 	</div>
 </template>
---
+++ src/components/partials/ListItem.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+</script>
+
+<template>
+	<h4 class="rs__sidebar-title">
+		<span class="rs__sidebar-title-note-icon material-symbols-sharp">notes</span>
+		All Note
+	</h4>
+	<ul class="rs__note-list">
+		<li>
+			<p>Note one title</p>
+		</li>
+		<li>
+			<p>Note one title</p>
+		</li>
+		<li>
+			<p>Note one title</p>
+		</li>
+		<li>
+			<p>Note one title</p>
+		</li>
+	</ul>
+</template>
