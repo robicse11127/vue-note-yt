80554b8a4e300efe38962c44cd037bbfbba3aa30
robicse11127 <robizmailbox@gmail.com>
2023-08-17 06:25:34 +0600

    EP-5 done

--- src/components/partials/ListItem.vue
+++ src/components/partials/ListItem.vue
@@ -1,24 +1,17 @@
 <script setup>
-
+import { useNoteStore } from '@/stores/NoteStore';
+const noteStore = useNoteStore();
+const props = defineProps( [ 'notes', 'title', 'icon', 'type' ] );
 </script>
 
 <template>
-	<h4 class="rs__sidebar-title">
-		<span class="rs__sidebar-title-note-icon material-symbols-sharp">notes</span>
-		All Note
+	<h4 class="rs__sidebar-title" v-if="'list' === type">
+		<span class="rs__sidebar-title-note-icon material-symbols-sharp">{{ icon }}</span>
+		{{ title }}
 	</h4>
-	<ul class="rs__note-list">
-		<li>
-			<p>Note one title</p>
-		</li>
-		<li>
-			<p>Note one title</p>
-		</li>
-		<li>
-			<p>Note one title</p>
-		</li>
-		<li>
-			<p>Note one title</p>
+	<ul class="rs__note-list" v-if="'list' === type" >
+		<li v-for="note in notes" :key="note.id">
+			<p>{{ note.title }}</p>
 		</li>
 	</ul>
 </template>
---
+++ src/stores/NoteStore.js
@@ -0,0 +1,25 @@
+import { defineStore } from 'pinia';
+
+export const useNoteStore = defineStore( 'noteStore', {
+
+	state: () => ({
+		notes: [
+			{ id: 1, title: 'Note title one', conent: 'Note one content', timestamp: Date.now(), pinned: false },
+			{ id: 2, title: 'Note title two', conent: 'Note two content', timestamp: Date.now(), pinned: true },
+			{ id: 3, title: 'Note title three', conent: 'Note three content', timestamp: Date.now(), pinned: true },
+			{ id: 4, title: 'Note title four', conent: 'Note four content', timestamp: Date.now(), pinned: false },
+			{ id: 5, title: 'Note title five', conent: 'Note five content', timestamp: Date.now(), pinned: false },
+		]
+	}),
+	getters: {
+		allNotes: ( state ) => {
+			return state.notes;
+		},
+
+		pinnedNotes: ( state ) => {
+			return state.notes.filter( note => note.pinned );
+		}
+	},
+	
+
+} );
