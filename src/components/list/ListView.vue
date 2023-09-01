b0d915dd5b02b2c268bbe962da76dde877302a5e
robicse11127 <robizmailbox@gmail.com>
2023-09-01 19:52:52 +0600

    EP7 done

--- src/components/list/ListView.vue
+++ src/components/list/ListView.vue
@@ -1,7 +1,9 @@
 <script setup>
 import Sidebar from '@/components/list/Sidebar.vue';
+import AddNote from '@/components/common/AddNote.vue';
 </script>
 
 <template>
 	<Sidebar />
+	<AddNote />
 </template>
